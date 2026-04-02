6aaf97f5d9d5207be01028131717d41cabb87518
root <root@ip-172-31-2-29.ec2.internal>
2026-04-02 06:46:53 +0000

    manuall added to state

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tf-state-2026"
+    bucket = "terraform-tf-state-1995"
     key    = "user-data-jenkins-setup-lab/user-data-jenkins-setup-lab.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "demo" {
+  ami                         = "ami-01b14b7ad41e17ba4"
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  subnet_id                   = "subnet-01b11e7120bf91e19"
+  user_data                   = file("userdata.sh")
+  tags = {
+    Name = "user-data-jenkins-setup-lab"
+  }
+}
